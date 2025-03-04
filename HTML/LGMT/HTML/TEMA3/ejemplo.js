f6af49bfe98b0a71a8767631a5c564c8785344fb
R1ul09 <128465165+R1ul09@users.noreply.github.com>
2025-03-04 19:40:05 +0100

    VA

--- HTML/LGMT/HTML/TEMA3/ejemplo.js
+++ HTML/LGMT/HTML/TEMA3/ejemplo.js
@@ -1,20 +1,35 @@
+l// Guardamos el objeto document de jQuery en una variable para poder usarlo más fácilmente
 let obj_documento = $(document);
 
-obj_documento.ready(inicio)
+// Cuando el documento esté listo, se ejecutará la función inicio
+obj_documento.ready(inicio);
 
+// Creamos la funcion inicio
 function inicio() {
-    let obj_izq = $("#botonizq")
-    obj_izq.click(fn_click_izq)
-    let obj_der = $("#botonder")
-    obj_der.click(fn_click_der)
+    // Guardamos el botón con el id botonizq en una variable
+    let obj_izq = $("#botonizq");
+    // Cuando se haga clic en botonizq, se ejecuta la función fn_click_izq
+    // Que sera creada mas adelante
+    obj_izq.click(fn_click_izq);
+
+    // Guardamos el botón con el id botonder en una variable
+    let obj_der = $("#botonder");
+    // Cuando se haga clic en botonder, se ejecuta la función fn_click_der
+    // Que sera creada tambien mas adelante
+    obj_der.click(fn_click_der);
 }
 
+// Inicializamos la función fn_click_izq
 function fn_click_izq() {
-    let obj_div = $("#texto")
-    obj_div.fadeOut()
+    // Guardamos el elemento con el id texto en una variable
+    let obj_div = $("#texto");
+    // Ocultamos el elemento con un efecto de desvanecimiento, en este caso el fadeOut
+    obj_div.fadeOut();
 }
 
 function fn_click_der() {
-    let obj_div = $("#texto")
-    obj_div.fadeIn()
+    // Guardamos el elemento con id texto en una variable
+    let obj_div = $("#texto");
+    // Mostramos el elemento con un efecto de aparición, en este caso el fadeIn
+    obj_div.fadeIn();
 }
